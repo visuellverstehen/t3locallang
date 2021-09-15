feb3a7a0b00d712d8c6aaf6a0d5fdb64b85b43e8
Rune Piper <rune.piper@visuellverstehen.de>
2021-09-15 13:16:27 +0200

    Add support for PHP 8.0 and TYPO3 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,6 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
     module.tx_t3locallang {
--- ext_tables.php
+++ ext_tables.php
@@ -1,22 +1,23 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+declare(strict_types=1);
 
-if (TYPO3_MODE === 'BE') {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'VV.' . $_EXTKEY,
-        'tools',
-        'Pi1',
-        '',
-        [
-            'Analyze' => 'index, collect, export',
-        ],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:t3locallang/Resources/Public/Icons/module.svg',
-            'labels' => 'LLL:EXT:t3locallang/Resources/Private/Language/locallang.xlf',
-        ]
-    );
-}
+use VV\T3locallang\Controller\AnalyzeController;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die();
+
+ExtensionUtility::registerModule(
+    'T3locallang',
+    'tools',
+    'Pi1',
+    '',
+    [
+        AnalyzeController::class => 'index, collect, export',
+    ],
+    [
+        'access' => 'admin',
+        'icon' => 'EXT:t3locallang/Resources/Public/Icons/module.svg',
+        'labels' => 'LLL:EXT:t3locallang/Resources/Private/Language/locallang.xlf',
+    ]
+);
